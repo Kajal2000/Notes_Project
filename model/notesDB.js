f3cd16abc2945d9eafd37eeab214acd5f8263f01
kajal <you@example.com>
2020-02-20 02:07:37 +0530

    comments added

--- model/notesDB.js
+++ model/notesDB.js
@@ -1,24 +1,27 @@
 let knex = require('../connection.js')
 
+// post data
 let insert_data = (data) => {
     return knex("Notes").insert(data)
 }
 
+// update data
 let update_data = (data,Notes_id) => {
     return knex("Notes").update(data).where("Notes.Notes_id",Notes_id)
 }
 
+// delete data
 let del_data = (Notes_id) => {
     return knex("Notes")
     .where("Notes.Notes_id",Notes_id)
     .del()
 }
-
+// get all data
 let get_data = ()=>{
     return knex.select("*")
     .from("Notes")
 }
-
+//get_id_by data
 let getby_id = (Notes_id)=>{
     return knex.select("*").from("Notes")
     .where("Notes.Notes_id",Notes_id)
